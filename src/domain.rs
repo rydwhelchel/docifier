acca448db94c994fdc357ec209760c7427793716
Ryan Whelchel <rydwhelchel@gmail.com>
2023-11-18 11:12:22 -0500

    Renamed Subjects->Targets, added branching path logic

--- src/domain.rs
+++ src/domain.rs
@@ -1,57 +1,69 @@
 use std::convert::Infallible;
-use clap::{Parser, parser};
+use clap::{Parser, parser, Subcommand};
 
-/// Object which contains comma separated list of subjects to promote
+/// Object which contains comma separated list of targets to promote
 #[derive(Debug, Clone)]
-pub struct Subjects {
+pub struct Targets {
     //TODO: Could also be nice to have an error message when the user inputs 1 or more images 
     //      without a colon in it (indicating they didn't specify the object version)
     //      note this error would only appear if the promotion_type is images
-    subjects: Vec<String>
+    targets: Vec<String>
 }
 
-impl Subjects {
-    fn new() -> Subjects {
-        Subjects { subjects: Vec::new() }
+impl Targets {
+    fn new() -> Targets {
+        Targets { targets: Vec::new() }
     }
 
     fn push(&mut self, s: String) {
-        self.subjects.push(s);
+        self.targets.push(s);
     }
 }
 
-impl std::fmt::Display for Subjects {
+impl std::fmt::Display for Targets {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut string_builder = String::new(); 
         // Hacky .clone() shenanigans. Aim to clean up
-        for s in self.subjects.clone().into_iter() {
+        for s in self.targets.clone().into_iter() {
             if string_builder.len() > 0 {
                 string_builder.push_str(", ");
             }
             string_builder.push_str(&s.clone());
         }
-        write!(f, "Subjects: [{}]", string_builder)
+        write!(f, "Targets: [{}]", string_builder)
     }
 }
 
-impl TryFrom<&str> for Subjects {
+impl TryFrom<&str> for Targets {
     type Error = Infallible; // If not infallible, there is an error I made for this in dead_code
 
     fn try_from(s: &str) -> Result<Self, Self::Error> {
         let seperated_list = s.split(',');
-        let mut subjects: Subjects = Subjects::new();
+        let mut targets: Targets = Targets::new();
         for item in seperated_list {
-            subjects.push(item.to_string());
+            targets.push(item.to_string());
         }
-        Ok(subjects)
+        Ok(targets)
     }
 }
 
-impl std::str::FromStr for Subjects {
+impl std::str::FromStr for Targets {
     type Err = Infallible;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Subjects::try_from(s)
+        Targets::try_from(s)
     }
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Prompt,
+    Line(OneLineArguments)
+}
+
+#[derive(Parser)]
+pub struct CliArguments {
+    #[command(subcommand)]
+    pub command: Commands,
 }
 
 /// Contains all information passed to the CLI on initial call
@@ -59,40 +71,47 @@ impl std::str::FromStr for Subjects {
 ///     Source: name of the source environment which we want to promote from
 ///     Destination: name of the destination environment which we want to promote to
 ///     PromotionType: Type that is getting promoted (object/image)
-///     Subjects: comma separated list of objects/images which we want to promote
-//TODO: Need to research Clap::parser to see if we can go down an alternate logic path
-//      when no args are passed
+///     Targets: comma separated list of objects/images which we want to promote
 #[derive(Parser, Debug)]
-pub struct CliArguments {
+pub struct OneLineArguments {
+    #[arg(short, long)]
     pub instance: String,
+
+    #[arg(short, long)]
     pub source: String,
+
+    #[arg(short, long)]
     pub destination: String,
+
     // Temporarily a string, might be worth making this an Enum? (if I can figure it out)
-    // Should have `images`, `config maps`, `secrets`, potentially `templates`
+    // Should have `images`, `config-maps`, `secrets`, potentially `templates`
+    #[arg(short, long)]
     pub promotion_type: String, 
-    pub subjects: Subjects
+
+    #[arg(short, long)]
+    pub targets: Targets
 }
 
-impl std::fmt::Display for CliArguments {
+impl std::fmt::Display for OneLineArguments {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f,
             "(Instance {}, {}->{}, of type {}, [{}])",
             &self.instance,
             &self.source, &self.destination,
             &self.promotion_type,
-            &self.subjects
+            &self.targets
         )
     }
 }
 
 
 #[test]
-fn test_subjects() {
-    //Subjects error type is infallible, so this test _should_ be pointless
-    let subjects = Subjects::try_from("asdf,fdsa,1234,4321").unwrap();
+fn test_targets() {
+    //Targets error type is infallible, so this test _should_ be pointless
+    let targets = Targets::try_from("asdf,fdsa,1234,4321").unwrap().targets;
     //Disgusting test...
-    assert_eq!(&"asdf".to_string(), subjects.subjects.get(0).unwrap());
-    assert_eq!(&"fdsa".to_string(), subjects.subjects.get(1).unwrap());
-    assert_eq!(&"1234".to_string(), subjects.subjects.get(2).unwrap());
-    assert_eq!(&"4321".to_string(), subjects.subjects.get(3).unwrap())
+    assert_eq!(&"asdf".to_string(), targets.get(0).unwrap());
+    assert_eq!(&"fdsa".to_string(), targets.get(1).unwrap());
+    assert_eq!(&"1234".to_string(), targets.get(2).unwrap());
+    assert_eq!(&"4321".to_string(), targets.get(3).unwrap())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 #![allow(unused, dead_code)]
 
+use read_input::prelude::*;
+
 use log::{info, warn, debug};
 use clap::{Parser};
 
 mod domain;
 mod utils;
-use crate::domain::Subjects;
+use crate::domain::Targets;
 use crate::domain::CliArguments;
+use crate::domain::OneLineArguments;
+use crate::domain::Commands;
 use crate::utils::initialize_logger;
 
+
+fn test_command() {
+    println!("Tested this command MFER!")
+}
 
 //TODO:
 // begin commenting structs to describe their purpose
 fn main() {
     initialize_logger();
     let args = CliArguments::parse();
-    debug!("Arguments provided: {:?}", args);
-    println!("Nice subjects bro!! {}", args.subjects)
+    match &args.command {
+        Commands::Prompt => {
+            println!("ooblah")
+        },
+        Commands::Line(one) => {
+            debug!("Arguments provided: {:?}", one);
+            println!("Nice targets bro!! {}", one.targets);
+            print!("read input test: ");
+            let targets = input::<Targets>().get();
+            println!("Nice input bro: {}", targets);
+        }
+    }
 }
